b3d57d68fb21d0ffe721a07cc763b30e2b15a89f
Albin Axtelius <albin@axtelius.se>
2023-04-14 14:31:09 +0200

    Added day 6

---
+++ lib/2020/day_6/solution_1.ex
@@ -0,0 +1,27 @@
+alias AdventOfCode.Helpers
+
+defmodule Solutions_2020.Day_6.Part_1 do
+  @spec solve_group([String.t()]) :: non_neg_integer()
+  def solve_group(group) do
+    group
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.reduce(&MapSet.union(&1, &2))
+    |> MapSet.size()
+  end
+
+  @spec solve(String.t()) :: non_neg_integer()
+  def solve(input) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(&String.split(&1, "\n"))
+    |> Enum.map(&solve_group/1)
+    |> Enum.sum()
+  end
+
+  def run() do
+    Helpers.read_input("day_6", "2020")
+    |> solve()
+    |> IO.puts()
+  end
+end
